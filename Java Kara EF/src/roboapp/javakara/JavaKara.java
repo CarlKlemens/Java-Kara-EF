4f8bf25ae8faff45c5b92eda5abee8e02d23315c
CarlKlemens <gofyourself1337@gmail.com>
2014-08-25 20:39:05 +0200

    Initial commit

---
+++ Java Kara EF/src/roboapp/javakara/JavaKara.java
@@ -0,0 +1,44 @@
+package roboapp.javakara;
+
+/**
+ * DIESE KLASSE KANN VON LESERN IGNORIERT WERDEN.
+ * 
+ * Dies sind nicht die richtigen Kara Methoden. Dies ist lediglich eine Hilfsklasse,
+ * um Fehlermeldungen in der Entwicklung vorzubeugen.
+ * 
+ * @author Carl Klemens
+ *
+ */
+public final class JavaKara {
+	
+	public final void move() {}
+	
+	public final boolean mushroomFront() {
+		return false;
+	}
+	
+	public final boolean onLeaf() {
+		return false;
+	}
+	
+	public final void putLeaf() {}
+	
+	public final void removeLeaf() {}
+	
+	public final boolean treeFront() {
+		return false;
+	}
+	
+	public final boolean treeLeft() {
+		return false;
+	}
+	
+	public final boolean treeRight() {
+		return false;
+	}
+	
+	public final void turnLeft() {}
+	
+	public final void turnRight() {}
+
+}
---
+++ Java Kara EF/src/roboapp/javakara/JavaKaraProgram.java
@@ -0,0 +1,18 @@
+package roboapp.javakara;
+
+/**
+ * DIESE KLASSE KANN VON LESERN IGNORIERT WERDEN.
+ * 
+ * Dies sind nicht die richtigen Kara Methoden. Dies ist lediglich eine Hilfsklasse,
+ * um Fehlermeldungen in der Entwicklung vorzubeugen.
+ * 
+ * @author Carl Klemens
+ *
+ */
+public abstract class JavaKaraProgram {
+	
+	protected final JavaKara kara = null;
+
+	protected abstract void myProgram();
+	
+}
